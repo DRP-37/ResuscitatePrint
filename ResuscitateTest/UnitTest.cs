e72c9f74db8c34e5a2c1d8900228cf85195bfa34
RobJBarr <robjb12@gmail.com>
2021-06-04 03:25:12 +0100

    Adding Resuscitate test folder

---
+++ ResuscitateTest/UnitTest.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
+using Resuscitate;
+
+namespace ResuscitateTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [UITestMethod]
+        public void TestMethod1()
+        {
+            MainPage mp = new MainPage();
+        }
+    }
+}
