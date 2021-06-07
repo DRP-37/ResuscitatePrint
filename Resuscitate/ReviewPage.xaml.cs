185d0700d08875f692a6bae890e369ebf49e3a97
RobJBarr <robjb12@gmail.com>
2021-06-07 18:25:47 +0100

    Merge initialFrontEnd into master

---
+++ Resuscitate/ReviewPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Resuscitate
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ReviewPage : Page
+    {
+        public ReviewPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            if (rootFrame.CanGoBack)
+            {
+                rootFrame.GoBack();
+            }
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FinishButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ResuscitateTest/Timing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace Resuscitate
+{
+    class Timing
+    {
+        public bool IsSet { get; set; }
+        public int Offset { get; set; }
+        public int Count { get; set; }
+
+        public override string ToString()
+        {
+            string minsStr, secsStr;
+
+            int Time = Count + Offset;
+            int mins = Time / 60;
+            int secs = Time % 60;
+
+            minsStr = mins < 10 ? "0" + mins.ToString() : mins.ToString();
+            secsStr = secs < 10 ? "0" + secs.ToString() : secs.ToString();
+
+            return minsStr + ":" + secsStr;
+        }
+    }
+}
