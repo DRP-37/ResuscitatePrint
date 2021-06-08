c5d98af26b0c09143b46992ba1f33c3db7b85a68
RobJBarr <robjb12@gmail.com>
2021-06-09 00:52:58 +0100

    Added more data classes for different pages

--- DataClasses/InitialAssessment.cs
+++ DataClasses/InitialAssessment.cs
@@ -6,7 +6,76 @@ using System.Threading.Tasks;
 
 namespace Resuscitate.DataClasses
 {
-    class InitialAssessment
+    public enum TemperatureReg { 
+        Dry,
+        PretermBag,
+        TermBag
+    }
+    public enum CordClamping { 
+        Immediate,
+        Now,
+        Delayed
+    }
+    internal class InitialAssessment : Event
     {
+        ApgarScore Apgar;
+        TemperatureReg TempReg;
+        CordClamping Clamping;
+        TimeSpan Time;
+
+        internal ApgarScore Apgar { get => Apgar; set => Apgar = value; }
+        public TemperatureReg TempReg { get => TempReg; set => TempReg = value; }
+        public CordClamping Clamping { get => Clamping; set => Clamping = value; }
+        public TimeSpan Time { get => Time; set => Time = value; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Initial Assessment at " +);
+            sb.Append('\t' + Apgar.toString() + '\n');
+            sb.Append('\t' + TempToString(TempReg) + '\n');
+            sb.Append('\t' + CordClampToString(Clamping));
+            if (Clamping == CordClamping.Now)
+            {
+                sb.Append(" " + Time.ToString() + '\n');
+            }
+            else {
+                sb.Append('\n');
+            }
+
+            return sb.ToString();
+        }
+        private String TempToString(TemperatureReg temp) {
+            switch (temp)
+            {
+                case TemperatureReg.Dry:
+                    return "Dry and Wrap";
+                    break;
+                case TemperatureReg.PretermBag:
+                    return "Bag (Preterm)";
+                    break;
+                case TemperatureReg.TermBag:
+                    return "Bag (Term)";
+                    break;
+                default: 
+                    return "";
+                    break;
+
+            }
+        }
+
+        private String CordClampToString(CordClamping clamping) {
+            switch (clamping)
+            {
+                case CordClamping.Immediate:
+                    return "Immediate";
+                case CordClamping.Delayed:
+                    return "Delayed";
+                case CordClamping.Now:
+                    return "Delayed";
+                default:
+                    return "";
+            }
+        }
     }
 }
