25e8955a89a3e9f63c164f9bf4d40be65f9fec6d
RobJBarr <robjb12@gmail.com>
2021-06-09 17:03:31 +0100

    Finished Observation class

--- DataClasses/Observation.cs
+++ DataClasses/Observation.cs
@@ -8,10 +8,20 @@ namespace Resuscitate.DataClasses
 {
     class Observation : Event
     {
+        
+
+        public float OximeterOxygen { get => OximeterOxygen; set => OximeterOxygen = value; }
+        public float Hr { get => Hr; set => Hr = value; }
+        public float OxygenGiven { get => OxygenGiven; set => OxygenGiven = value; }
+        public TimeSpan Time { get => Time; set => Time = value; }
+
         public override string ToString()
         {
             StringBuilder sb = new StringBuilder();
-
+            sb.Append("Observation at " + Time.ToString() + ":\n");
+            sb.Append("\tHeart Rate (bpm): " + Hr.ToString() + '\n');
+            sb.Append("\tOximeter Oxygen Level: " + OximeterOxygen.ToString() + "%\n");
+            sb.Append("\tPercentage Oxygen Given: " + OxygenGiven.ToString() + "%\n");
             return sb.ToString();
         }
     }
