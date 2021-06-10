ca74ad8df4b31ed0972aae79ad3c490a44c97115
RobJBarr <robjb12@gmail.com>
2021-06-10 14:09:24 +0100

    Committing event and timing class

---
+++ DataClasses/EventAndTiming.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resuscitate.DataClasses
+{
+    class EventAndTiming
+    {
+        public EventAndTiming(Timing t, Event e) {
+            Timing = t;
+            MedicalEvent = e;
+        }
+
+        public Timing Timing { get; set; }
+        internal Event MedicalEvent { get; set; }
+    }
+}
