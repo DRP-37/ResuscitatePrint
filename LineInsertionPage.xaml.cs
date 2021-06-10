fbb88ee8f2fb3678088d0aef4b5afb27277f82f3
Nicole Obretincheva <no119@ic.ac.uk>
2021-06-10 17:44:19 +0100

    Implemented UI for Line Insertion

--- LineInsertionPage.xaml.cs
+++ LineInsertionPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -23,10 +24,17 @@ namespace Resuscitate
     public sealed partial class LineInsertionPage : Page
     {
         public Timing TimingCount { get; set; }
+        private Button[] insertion;
+        // Airway Positioning:
+        // 0: Umbilical
+        // 1: Intraosseous
+        private int lineInsertion;
+        private bool isSuccessful;
 
         public LineInsertionPage()
         {
             this.InitializeComponent();
+            insertion = new Button[] { Intraosseous, Umbilical };
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -39,7 +47,14 @@ namespace Resuscitate
 
         private void ConfirmButton_Click(object sender, RoutedEventArgs e)
         {
-
+            // set data structure with line insertion, whether it was successful
+            // and time stamp of selection
+            // if a selection has not been made do not allow confirm
+            if (SelectionMade(insertion) && 
+                SelectionMade(new Button[] { Successful, Unsuccessful}))
+            {
+                Frame.Navigate(typeof(Resuscitation), TimingCount);
+            }
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
@@ -55,6 +70,42 @@ namespace Resuscitate
         private void TimeView_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+
+        private void Successful_Click(object sender, RoutedEventArgs e)
+        {
+            Button curr = sender as Button;
+            UpdateColours(new Button[] {Successful, Unsuccessful}, curr);
+            isSuccessful = curr.Equals(Successful);
+        }
+
+        private void Insertion_Click(object sender, RoutedEventArgs e)
+        {
+            Button curr = sender as Button;
+            UpdateColours(insertion, curr);
+            lineInsertion = Array.IndexOf(insertion, curr);
+        }
+
+        private void UpdateColours(Button[] buttons, Button sender)
+        {
+            buttons[0].Background = new SolidColorBrush(Colors.White);
+            buttons[1].Background = new SolidColorBrush(Colors.White);
+            sender.Background = new SolidColorBrush(Colors.LightGreen);
+        }
+
+        private bool SelectionMade(Button[] buttons)
+        {
+            foreach (Button button in buttons)
+            {
+                SolidColorBrush colour = button.Background as SolidColorBrush;
+
+                if (colour.Color == Colors.LightGreen)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
