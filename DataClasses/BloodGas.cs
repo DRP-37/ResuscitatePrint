5ceac8015c0a878a85cda37e681335a0875c7caf
Euan Scott-Watson <37578825+EuanScottWatson@users.noreply.github.com>
2021-06-10 15:51:30 +0100

    Added a dataclass for blood/gas

---
+++ DataClasses/BloodGas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resuscitate.DataClasses
+{
+    class BloodGas : Event
+    {
+        private Timing time;
+        private float pH;
+        private float lactate;
+        private float glucose;
+        public Timing Time { get => time; set => time = value; }
+        public float PH { get => pH; set => pH = value; }
+        public float Lactate { get => lactate; set => lactate = value; }
+        public float Glucose { get => glucose; set => glucose = value; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append($"Timing: {time.ToString()}, pH: {pH.ToString()}, Lactate: {lactate.ToString()}, Glucose: {glucose.ToString()}");
+
+            return sb.ToString();
+        }
+
+    }
+}
