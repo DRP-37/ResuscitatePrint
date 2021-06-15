8eef90a4bd00958918c0e489c2c00539df01cc72
umiralles <unamiralles@hotmail.co.uk>
2021-06-15 20:40:29 +0100

    Added basic txt file creation for the report

--- ReviewPage.xaml.cs
+++ ReviewPage.xaml.cs
@@ -15,6 +15,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Graphics.Printing;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -34,7 +35,7 @@ namespace Resuscitate
             this.InitializeComponent();
         }
 
-        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             // Take value from previous screen
             var RDaT = (ReviewDataAndTiming)e.Parameter;
@@ -104,6 +105,28 @@ namespace Resuscitate
         private void StaffInfo_Click(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(StaffPage), TimingCount);
+        }
+
+        private async void ExportButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Create sample file with the ID of the patient; replace if exists.
+            Windows.Storage.StorageFolder storageFolder =
+                Windows.Storage.ApplicationData.Current.LocalFolder;
+            Windows.Storage.StorageFile sampleFile = 
+                await storageFolder.CreateFileAsync("sample.txt", Windows.Storage.CreationCollisionOption.ReplaceExisting);
+            String doc = "Resuscitation Report for patient: " + patientData.Id + "\n";
+            foreach (StatusEvent statEvent in StatusList.Events)
+            {
+                doc += statEvent.Event + " " + statEvent.Data + " " + statEvent.Time + ";\n";
+            }
+            await Windows.Storage.FileIO.WriteTextAsync(sampleFile, doc);
+
+            ExportButton.Background = new SolidColorBrush(Colors.LightGreen);
+            // DEBUG 
+            // Storage­File.Get­File­From­Application­Uri­Async. - get from URI address
+            // Directory.GetCurrentDirectory 
+            // if a path is not selected by user default to installation path of the app
+            System.Diagnostics.Debug.WriteLine(String.Format("File is located at {0}", sampleFile.Path.ToString()));
         }
     }
 }
