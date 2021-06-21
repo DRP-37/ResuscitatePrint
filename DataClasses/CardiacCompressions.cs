e49fced389dd5307c2986b22bd13b2cab782f997
Euan Scott-Watson <37578825+EuanScottWatson@users.noreply.github.com>
2021-06-21 21:46:19 +0100

    Fixed compressions

--- DataClasses/CardiacCompressions.cs
+++ DataClasses/CardiacCompressions.cs
@@ -8,44 +8,15 @@ namespace Resuscitate.DataClasses
 {
     public class CardiacCompressions : Event
     {
-        private List<Timing> start = new List<Timing>();
-        private List<Timing> end = new List<Timing>();
+        private Timing time;
+        private string length;
 
-        public void startedCompressions(Timing time)
-        {
-            start.Add(time);
-        }
-
-        public void stopCompressions(Timing time)
-        {
-            end.Add(time);
-        }
+        public Timing Time { get => time; set => time = value; }
+        public string Length { get => length; set => length = value; }
 
         override public string ToString()
         {
-            StringBuilder sb = new StringBuilder();
-
-            sb.Append("Timings of cardiac compressions: \n");
-
-            if (end.Count > start.Count)
-            {
-                sb.Append("- " + end[end.Count - 1]);
-            }
-
-            for (int i = 0; i < start.Count; i++)
-            {
-                if (i < end.Count)
-                {
-                    sb.Append(start[i].ToString() + " - " + end[i].ToString() + "\n");
-                }
-            }
-
-            if (start.Count > end.Count)
-            {
-                sb.Append(start[start.Count - 1] + " -");
-            }
-
-            return sb.ToString();
+            return $"Cardiac compression at: {time.ToString()} for {length} seconds\n";
         }
     }
 }
